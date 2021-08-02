da22992355a4f9ad71d370ac049bf1ddacd0068a
Naman Sachdeva <NAMAN.SACHDEVA@msitjanakpuri.co.in>
2021-08-02 18:04:17 +0530

    Have shown total score and a message in result

--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    var resultText = 'You did it';
+    if (resultScore >= 30)
+      resultText = 'You are Normal';
+    else if (resultScore >= 20)
+      resultText = 'You are Pretty Decent';
+    else if (resultScore >= 15)
+      resultText = 'You have a Slightly Different Personality';
+    else
+      resultText = 'You are Strange!';
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("You Did It"),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Score : $resultScore',
+            style: TextStyle(
+                fontSize: 50.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 20.0),
+          Text(
+            resultPhrase,
+            style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 20.0),
+          ElevatedButton(
+            onPressed: resetHandler,
+            child: Text('Restart Quiz'),
+          )
+        ],
+      ),
     );
   }
 }
